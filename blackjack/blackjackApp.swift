4a93a587c49c77689f7d23adaa6cab319f22a0cf
oscar.nowell <oscar.nowell@gmail.com>
2025-10-15 17:38:35 +0100

    Initial Commit

---
+++ blackjack/blackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackjackApp.swift
+//  blackjack
+//
+//  Created by Oscar Nowell on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct blackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
